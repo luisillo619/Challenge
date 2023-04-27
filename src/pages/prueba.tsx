7552209becbae2ba2e26db982358b6a1d3b255cf
luisillo619 <luiscarlosrangel619@gmail.com>
2023-04-26 19:01:11 -0600

    FIX/bug fix chart

---
+++ src/pages/prueba.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const prueba = () => {
+  return (
+    <div>
+ <h1>pepe</h1>
+ <button disabled={true}>holaaa</button>
+    </div>
+   
+  )
+}
+
+export default prueba
